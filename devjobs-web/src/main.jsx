10290db377764b9f23e79ea84792b3112a27fe29
luulichma <chiennguyenthe659@gmail.com>
2026-04-12 13:43:49 +0700

    update

--- devjobs-web/src/main.jsx
+++ devjobs-web/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const GOOGLE_CLIENT_ID = '856613487614-l7e3joh7e0mfmhue9m0g89k7lom8o8i3.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '636369793383-s92s646d2ugp33t2cf46jk6i57v02i00.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
